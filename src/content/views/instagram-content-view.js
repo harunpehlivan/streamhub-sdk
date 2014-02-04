5853ee899d8155cf2eb91c748b3d44ee59193bb9
Ryan Cheung <ryanc@livefyre.com>
2014-02-04 14:19:02 -0800

    Add buttons programatically to each content source view

--- src/content/views/instagram-content-view.js
+++ src/content/views/instagram-content-view.js
@@ -24,12 +24,37 @@ function (LivefyreContentView, inherits) {
         }
     });
 
+    InstagramContentView.prototype.render = function () {
+        LivefyreContentView.prototype.render.call(this);
+
+        if (! this._rendered) {
+            var likeButton = new Button(undefined, {
+                elClassPrefix: 'hub',
+                className: 'hub-content-like'
+            });
+            var shareButton = new Button(undefined, {
+                elClassPrefix: 'hub',
+                className: 'hub-btn-link hub-content-share',
+                label: 'Share'
+            });
+
+            this.addButton(likeButton);
+            this.addButton(shareButton);
+        } else {
+            for (var i=0; i < this._controls['left'].length; i++) {
+                this.addButton(this._controls['left'][i]);
+            }
+        }
+
+        this._rendered = true;
+    };
+
     /**
      * Gets the template rendering context. By default, returns "this.content".
      * @return {Content} The content object this view was instantiated with.
      */
     InstagramContentView.prototype.getTemplateContext = function () {
-        var context = LivefyreContentView.prototype.getTemplateContext.call(this);
+        var context = ContentView.prototype.getTemplateContext.call(this);
 
         context.authorDisplayName = context.author.displayName;
 
