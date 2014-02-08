b21cec8d1877b7ed2c14d5668a18715dd163785b
Ryan Cheung <ryanc@livefyre.com>
2014-02-07 17:10:01 -0800

    Add HubToggleButton for like button. Update StateToContent to handle v3.0-style opine.

--- src/ui/button/hub-button.js
+++ src/ui/button/hub-button.js
@@ -1,10 +1,20 @@
 'use strict';
 
 var Button = require('streamhub-sdk/ui/button');
+var Command = require('streamhub-sdk/ui/command');
 var inherits = require('inherits');
 
-function HubButton (command, opts) {
-    opts.elClassPrefix = 'hub';
+function HubButton (fnOrCommand, opts) {
+    opts = opts || {};
+    opts.elClassPrefix = opts.elClassPrefix || '';
+    opts.elClassPrefix += ' hub';
+
+    var command;
+    if (typeof(fnOrCommand) === 'function') {
+        command = new Command(fnOrCommand);
+    } else if (fnOrCommand) {
+        command = fnOrCommand;
+    }
     Button.call(this, command, opts);
 };
 inherits(HubButton, Button);
--- tests/spec/content/views/content-view.js
+++ tests/spec/content/views/content-view.js
@@ -246,5 +246,76 @@ function (
             });
         });
 
+        describe('Like button', function () {
+
+            it("only renders for non-Twitter content items", function () {
+                var contentViewFactory = new ContentViewFactory();
+
+                var twitterContent = new LivefyreTwitterContent({
+                    id: 'tweet-1234@twitter.com',
+                    body: 'tweet i am',
+                    author: {
+                        id: 'jimmy@twitter.com'
+                    }
+                });
+                var twitterContentView = contentViewFactory.createContentView(twitterContent);
+                twitterContentView.render();
+
+                expect(twitterContentView.$el.find('.hub-content-like')).toHaveLength(0);
+
+                var lfContent = new LivefyreContent({ body: 'lf content' });
+                var lfContentView = contentViewFactory.createContentView(lfContent);
+                lfContentView.render();
+
+                expect(lfContentView.$el.find('.hub-content-like')).toHaveLength(1);
+            });
+
+            describe('when Like button clicked', function () {
+                var content,
+                    contentView,
+                    likeButtonEl;
+
+                beforeEach(function () {
+                    content = new Content({ body: 'what' });
+                    contentView = new ContentView({ content: content });
+                    contentView.render();
+                    likeButtonEl = contentView.$el.find('.hub-content-like');
+                });
+
+                afterEach(function () {
+                    $('body').off();
+                });
+
+                it("lazily attaches an event listener for 'contentLike.hub' event on body element", function () {
+                    expect($._data($('body')[0], 'events')).toBe(undefined);
+                    likeButtonEl.trigger('click');
+                    expect($._data($('body')[0], 'events').contentLike.length).toBe(1);
+                });
+
+                it("sets #_likeRequestListener flag to true", function () {
+                    expect(contentView._likeRequestListener).toBe(false);
+                    likeButtonEl.trigger('click');
+                    expect(contentView._likeRequestListener).toBe(true);
+                });
+            });
+
+            describe("body element 'contentLike.hub' listener", function () {
+                var content,
+                    contentView,
+                    likeButtonEl;
+
+                beforeEach(function () {
+                    content = new Content({ body: 'what' });
+                    contentView = new ContentView({ content: content });
+                    contentView.render();
+                    likeButtonEl = contentView.$el.find('.hub-content-like');
+                });
+
+                afterEach(function () {
+                    $('body').off();
+                });
+            });
+        });
+
     });
 });
