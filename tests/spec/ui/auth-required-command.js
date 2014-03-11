301f51993355ce5d733d1b27ea00d322c35fbaa0
Joao-S-Martins <joao@livefyre.com>
2014-03-11 16:45:14 -0700

    Resurected the auth-req-comm tests from the dead.

--- tests/spec/ui/auth-required-command.js
+++ tests/spec/ui/auth-required-command.js
@@ -3,77 +3,89 @@
 var auth = require('auth');
 var MockUserFactory = require('livefyre-auth-tests/mocks/mock-user-factory');
 var Auth = require('streamhub-sdk/auth');
-var AuthRequiredCommand = require('streamhub-sdk/ui/auth-required-command');
+var AuthReqCommand = require('streamhub-sdk/ui/auth-required-command');
 var Command = require('streamhub-sdk/ui/command');
 var EventEmitter = require('event-emitter');
 
-describe('AuthRequiredCommand: streamhub-sdk/ui/command/auth-required-command', function () {
+describe('streamhub-sdk/ui/command/auth-required-command', function () {
     it('is a constructor that subclasses Command', function () {
-        expect(typeof(AuthRequiredCommand)).toBe('function');
-        var authReqCmd = new AuthRequiredCommand(new Command(function () {}));
-        expect(authReqCmd instanceof AuthRequiredCommand).toBe(true);
+        expect(typeof(AuthReqCommand)).toBe('function');
+        var authReqCmd = new AuthReqCommand(new Command(function () {}));
+        expect(authReqCmd instanceof AuthReqCommand).toBe(true);
         expect(authReqCmd instanceof Command).toBe(true);
         expect(authReqCmd instanceof EventEmitter).toBe(true);
     });
-
+    
+    it('can\'t be constructed without a specified command', function () {
+        expect(function () {
+            new AuthReqCommand();
+        }).toThrow();
+        expect(function () {
+            new AuthReqCommand(function () {});
+        }).toThrow();
+    });
+    
     describe('when constructed', function () {
+        var authCmd;
+        var authCmdSpy;
         var authReqCmd;
         var cmd;
         var cmdSpy;
         var observer;
-        var mockUserFactory;
-        var user;
-
         beforeEach(function () {
-            // Auth mock user
-            mockUserFactory = new MockUserFactory();
-            user = mockUserFactory.createUser();
-
             cmdSpy = jasmine.createSpy('command function');
             cmd = new Command(cmdSpy);
-            authReqCmd = new AuthRequiredCommand(cmd);
-            
+            authReqCmd = new AuthReqCommand(cmd);
             observer = jasmine.createSpy('change:canExecute');
             authReqCmd.on('change:canExecute', observer);
-
-            auth.delegate({
-                login: function () {}
-            });
-            auth.delegate({
-                login: function () {},
-                logout: function (loggedOut) {
-                    loggedOut();
-                }
-            });
+            
+            authCmdSpy = jasmine.createSpy('authentication command function')
+                .andCallFake(function (callback) {
+                    Auth.setToken('TOKENZ');
+                    callback();
+                });
+            authCmd = new Command(authCmdSpy);
         });
         
         afterEach(function () {
+            Auth.setToken('');
             authReqCmd._listeners && authReqCmd.removeListener('change:canExecute', observer);
-            auth.logout();
         });
         
         it('can be .destroy()\'d', function () {
             expect(typeof(authReqCmd.destroy)).toBe('function');
             
+            expect(authReqCmd._authCmd).not.toBeNull();
+            expect(authReqCmd._command).not.toBeNull();
+            expect(authReqCmd._execute).not.toBeNull();
             expect(authReqCmd._listeners).not.toBeNull();
             
             authReqCmd.destroy();
             
+            expect(authReqCmd._authCmd).toBeNull();
+            expect(authReqCmd._command).toBeNull();
+            expect(authReqCmd._execute).toBeNull();
             expect(authReqCmd._listeners).toBeNull();
         });
         
-        it('has a default ._authCmd that is enabled', function () {
+        it('has a default ._authCmd that is disabled', function () {
             expect(authReqCmd._authCmd).toBeTruthy();
-            expect(authReqCmd._authCmd.canExecute()).toBe(true);
+            expect(authReqCmd._authCmd.canExecute()).toBe(false);
         });
         
-        it('listens to auth for "authenticate.livefyre" and emits change:canExecute', function () {
+        it('can .setAuthCommand()', function () {
+            authReqCmd.setAuthCommand(authCmd);
+            expect(authReqCmd._authCmd).toBe(authCmd);
+        });
+        
+        it('listens to Auth for "token" and emits change:canExecute', function () {
+            Auth.setToken('TOKENZ');
             expect(observer).toHaveBeenCalledWith(true);
         });
         
         describe('emits change:canExecute when ', function () {
-            it('detects a change in ._authCmd.canExecute()', function () {
-                authReqCmd._authCmd.disable();
+            it('detects a change in ._command.canExecute()', function () {
+                authReqCmd._command.disable();
                 expect(observer).toHaveBeenCalled();
             });
             it('detects a change in ._authCmd.canExecute()', function () {
@@ -81,75 +93,86 @@ describe('AuthRequiredCommand: streamhub-sdk/ui/command/auth-required-command', 
                 expect(observer).toHaveBeenCalled();
             });
             it('detects a change in the Auth token', function () {
+                Auth.setToken('TOKENZ');
                 expect(observer).toHaveBeenCalled();
             });
         });
         
         describe('and is .disable()\'d', function () {
-            it('sets ._canExecute === false and emits change:canExecute, false', function () {
+            it('disables ._command and emits change:canExecute, false', function () {
                 authReqCmd.disable();
                 
-                expect(cmd.canExecute()).toBe(true);
-                expect(authReqCmd._canExecute).toBe(false);
+                expect(cmd.canExecute()).toBe(false);
+                expect(authReqCmd.canExecute()).toBe(false);
             });
             
-            it('sets ._canExecute === true with .enable() and emits change:canExecute, false', function () {
+            it('can re-enable ._command with .enable() and emits change:canExecute, false', function () {
                 authReqCmd.disable();
                 
                 authReqCmd.enable();
                 
                 expect(cmd.canExecute()).toBe(true);
-                expect(authReqCmd._canExecute).toBe(true);
+//                debugger
+                expect(authReqCmd.canExecute()).toBe(Boolean(Auth.getToken() || authReqCmd._authCmd.canExecute()));
             });
         });
         
-        describe('and .canExecute()', function () {
+        describe('and ._command.canExecute()', function () {
             describe('but user isn\'t authenticated', function () {
                 beforeEach(function () {
-                    auth.logout();
+                    authReqCmd.setAuthCommand(authCmd);
                 });
-
-                it('auth.isAuthenticated() === false', function () {
-                    expect(auth.isAuthenticated()).toBe(false);
+                
+                describe('and the ._authCmd.canExecute()', function () {
+                    it('executes ._authCmd, passing a function to invoke on succes', function () {
+                        authReqCmd.execute();
+                        expect(authCmdSpy).toHaveBeenCalled();
+                        expect(cmdSpy).toHaveBeenCalled();
+                    });
+                    
                 });
-
-                describe('and #_canExecute === true', function () {
+                
+                describe('and the ._authCmd can\'t execute()', function () {
                     beforeEach(function () {
-                        authReqCmd._authCmd.enable();
+                        authCmd.disable();
                     });
-
-                    describe('and #_authCmd.canExecute() === true', function () {
-                        it('can execute', function () {
-                            authReqCmd._authCmd.enable();
-                            expect(authReqCmd._canExecute).toBe(true);
-                            expect(authReqCmd.canExecute()).toBe(true);
-                        });
-                    });
-
-                    describe('and #authCmd.canExecute === false', function () {
-                        it('cannot execute', function () {
-                            authReqCmd._authCmd.disable();
-                            expect(authReqCmd._canExecute).toBe(true);
-                            expect(authReqCmd.canExecute()).toBe(false);
-                        });
+                    
+                    it('can\'t .execute()', function () {
+                        expect(authReqCmd.canExecute()).toBe(false);
+                        authReqCmd.execute();
+                        expect(cmdSpy).not.toHaveBeenCalled();
                     });
                 });
             });
             
             describe('and user is authenticated', function () {
                 beforeEach(function () {
-                    auth.login({ livefyre: user});
-                });
-
-                it('auth.isAuthenticated() === true', function () {
-                    expect(auth.isAuthenticated()).toBe(true);
+                    Auth.setToken('TOKENZ');
                 });
                 
-                it('.execute()\'s ._authCmd', function () {
+                it('.execute()s ._command', function () {
                     expect(authReqCmd.canExecute()).toBe(true);
                     authReqCmd.execute();
                     expect(cmdSpy).toHaveBeenCalled();
                 });
+            });
+        });
+        
+        describe('with opts', function () {
+            var opts;
+            var cmd;
+            beforeEach(function () {
+                cmd = new Command(function () {});
+                opts = {
+                    authCmd: cmd
+                };
+            });
+            
+            it('passes opts.authCmd to .setAuthCommand()', function () {
+                var authReqCmd = new AuthReqCommand(
+                        new Command(function () {}),
+                        opts);
+                expect(authReqCmd._authCmd).toBe(cmd);
             });
         });
     });
