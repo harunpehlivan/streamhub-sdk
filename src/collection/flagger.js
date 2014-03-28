645f9d7005542618485bd2c2ab18a8a1e6192d07
Ryan Cheung <ryanc@livefyre.com>
2014-03-28 16:34:53 -0700

    Add jsdocs to flagger

--- src/collection/flagger.js
+++ src/collection/flagger.js
@@ -8,6 +8,17 @@ var Flagger = function (opts) {
     this._writeClient = opts.writeClient || new LivefyreWriteClient();
 };
 
+/**
+ * Flag a piece of content
+ * @param opts {Object} The livefyre collection options.
+ * @param opts.flagType {string} The flag type
+ *     (e.g. 'offensive', 'spam', 'disagree', 'off-topic')
+ * @param opts.contentId {string} The ID of the content to flag
+ * @param opts.collectionId {string} The livefyre collectionId for the conversation
+ * @param opts.lftoken {string} The livefyre user auth token
+ * @param callback {function} A callback that is called upon success/failure of the
+ *     write request. Callback signature is "function(error, data)".
+ */
 Flagger.prototype.flag = function (opts, callback) {
     this._writeClient.flag({
         network: opts.content.collection.network,
