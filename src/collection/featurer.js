13e1a96c675cb07a223afb5d4eb126340a670468
Ryan Cheung <ryanc@livefyre.com>
2014-03-28 16:28:01 -0700

    Add jsdocs annotations for featurer

--- src/collection/featurer.js
+++ src/collection/featurer.js
@@ -9,6 +9,13 @@ define([
         this._writeClient = opts.writeClient || new LivefyreWriteClient();
     };
 
+    /**
+     * Feature a content item
+     * @param content {Content} The content item to feature
+     * @param callback {function} The callback to be invoked upon
+     *    successful/failed feature request. Callback has the signature,
+     *    function(error , data)
+     */
     Featurer.prototype.feature = function (content, callback) {
         this._writeClient.feature({
             network: content.collection.network,
@@ -19,6 +26,13 @@ define([
         }, callback);
     };
 
+    /**
+     * Unfeature a content item
+     * @param content {Content} The content item to unfeature
+     * @param callback {function} The callback to be invoked upon
+     *    successful/failed unfeature request. Callback has the signature,
+     *    function(error , data)
+     */
     Featurer.prototype.unfeature = function (content, callback) {
         this._writeClient.unfeature({
             network: content.collection.network,
