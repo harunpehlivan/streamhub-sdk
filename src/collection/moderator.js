c1c27e0e2592f678bf44af5051e320ece9fd8231
Ryan Cheung <ryanc@livefyre.com>
2014-03-28 16:35:57 -0700

    Add jsdocs for moderator

--- src/collection/moderator.js
+++ src/collection/moderator.js
@@ -9,6 +9,16 @@ define([
         this._writeClient = opts.writeClient || new LivefyreWriteClient();
     };
 
+    /**
+     * Updates an existing piece of content in a Livefyre collection.
+     * @param opts {Object} Update content options
+     * @param opts.contentId {string} The ID of the content to trash
+     * @param opts.collectionId {string} The collection ID of the content
+     * @param opts.siteId {string} The site ID of the content
+     * @param opts.lftoken {string} The livefyre user auth token
+     * @param callback {function} A callback that is called upon sucess/failure
+     *     of the update request. Callback signature is "function(error, data)".
+     */
     Moderator.prototype.trash = function (content, callback) {
         this._writeClient.trashContent({
             contentId: content.id,
@@ -19,6 +29,17 @@ define([
         }, callback);
     };
 
+    /**
+     * Updates an existing piece of content in a Livefyre collection.
+     * @param opts {Object} Update content options
+     * @param opts.body {string} The body to update the content with
+     * @param opts.contentId {string} The ID of the content to update
+     * @param opts.collectionId {string} The collection ID of the content
+     * @param opts.siteId {string} The site ID of the content
+     * @param opts.lftoken {string} The livefyre user auth token
+     * @param callback {function} A callback that is called upon sucess/failure
+     *     of the update request. Callback signature is "function(error, data)".
+     */
     Moderator.prototype.edit = function (content, callback) {
         this._writeClient.updateContent({
             body: content.body,
