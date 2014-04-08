da5b90b0654018e4301771308ecc38c117852420
Joao-S-Martins <joao@livefyre.com>
2014-04-08 14:28:51 -0700

    Untested code, just to make sure brains are in sync.

--- src/collection/main.js
+++ src/collection/main.js
@@ -8,13 +8,13 @@ define([
     'streamhub-sdk/collection/clients/create-client',
     'streamhub-sdk/collection/clients/write-client',
     'streamhub-sdk/content/clients/content-client',
-    'streamhub-sdk/content/state-to-content',
+    'streamhub-sdk/content/fetchContent',
     'streamhub-sdk/auth',
     'inherits',
     'streamhub-sdk/debug'],
 function (CollectionArchive, CollectionUpdater, CollectionWriter, FeaturedContents,
         Duplex, LivefyreBootstrapClient, LivefyreCreateClient, LivefyreWriteClient,
-        LivefyreContentClient, StateToContent, Auth, inherits, debug) {
+        LivefyreContentClient, fetchContent, Auth, inherits, debug) {
     'use strict';
 
 
@@ -134,48 +134,7 @@ function (CollectionArchive, CollectionUpdater, CollectionWriter, FeaturedConten
         opts.collection = this;
         opts.contentId = contentId;
 
-        //Send the request
-        this._contentClient.getContent(opts, clbk);
-        
-        function clbk(err, data) {
-            if (err) {
-                callback(err);
-                return;
-            }
-            
-            var states = data.content || [],
-                state,
-                content,
-                contents = [];
-            
-            //Prepare StateToContents to handle the recieved states
-            opts.authors = data.authors;
-            var trans = new StateToContent(opts);
-            
-            //Listen for states that have been transformed into Content
-            trans.on('data', function (content) {
-                contents.push(content);
-
-                //Once we've recieved as many as we've written, find the desired
-                //Content and send it to the callback.
-                if (contents.length === statesCount) {
-                    for (var j=0; j < statesCount; j++) {
-                        if (contents[j] && contents[j].id === contentId) {//Must be strings
-                            callback(undefined, contents[j]);
-                            return;
-                        }
-                    }
-                    //If we get here, something went very wrong.
-                    callback('ERROR');
-                }
-            });
-
-            //Write each state into StateToContent
-            for (var i=0, statesCount=states.length; i < statesCount; i++) {
-                state = states[i];
-                trans.write(state);
-            }
-        }
+        fetchContent(opts, clbk);
     };
 
 
