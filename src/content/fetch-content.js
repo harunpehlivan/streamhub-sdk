dc75404c9611c548c8711288f6b5a49a3fbeb3ba
Joao-S-Martins <joao@livefyre.com>
2014-04-09 23:42:21 -0700

    Debugged and tested.

--- src/content/fetch-content.js
+++ src/content/fetch-content.js
@@ -13,6 +13,7 @@ var StateToContent = require('streamhub-sdk/content/state-to-content');
  * @param [opts.environment] {string=}
  * @param [opts.replies] {boolean=} Default is true.
  * @param [opts.depthOnly] {boolean=} Default is false.
+ * @param [opts.contentClient] {ContentClient=}
  * @param callback {!function(err: Object, data: Content)}
  */
 var fetchContent = function (opts, callback) {
@@ -26,13 +27,14 @@ var fetchContent = function (opts, callback) {
         throw 'Can\'t fetchContent() without specifying a callback';
     }
     //build opts for content client and state to content
-    opts.replies = opts.replies || true;
+    opts.replies = !!opts.replies;
     opts.depthOnly = opts.depthOnly || false;
 
     //Send the request
-    this._contentClient.getContent(opts, clbk);
+    var contentClient = opts.contentClient || new LivefyreContentClient();
+    contentClient.getContent(opts, processStates);
     
-    function clbk(err, data) {
+    function processStates(err, data) {
         if (err) {
             callback(err);
             return;
@@ -43,26 +45,27 @@ var fetchContent = function (opts, callback) {
             content,
             contents = [];
         
-        //Prepare StateToContents to handle the recieved states
+        //Prepare StateToContents to handle the received states
         opts.authors = data.authors;
         var trans = new StateToContent(opts);
         
         //Listen for states that have been transformed into Content
         trans.on('data', function (content) {
             contents.push(content);
-
-            //Once we've recieved as many as we've written, find the desired
-            //Content and send it to the callback.
-            if (contents.length === statesCount) {
-                for (var j=0; j < statesCount; j++) {
-                    if (contents[j] && contents[j].id === contentId) {//Must be strings
-                        callback(undefined, contents[j]);
-                        return;
-                    }
+        });
+        
+        trans.once('drain', function () {
+            //Once trans has processed everything, find the desired Content
+            //and send it to the callback.
+            for (var i=0; i < contents.length; i++) {
+                if (contents[i] && contents[i].id === opts.contentId) {//Must be strings
+                    callback(undefined, contents[i]);
+                    return;
                 }
-                //If we get here, something went very wrong.
-                callback('ERROR');
             }
+            
+            //If we get here, something went very wrong.
+            callback('ERROR');
         });
 
         //Write each state into StateToContent
