8701d0e44d215c0ca5f7234097f00364f9e4a478
Benjamin Goering <bengoering@gmail.com>
2014-04-21 23:20:44 -0700

    Export ContentListView

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ define([
     'streamhub-sdk/jquery',
     'text!streamhub-sdk/version.txt',
     'streamhub-sdk/collection',
-    'streamhub-sdk/content'],
-function($, version, Collection, Content) {
+    'streamhub-sdk/content',
+    'streamhub-sdk/content/views/content-list-view'],
+function($, version, Collection, Content, ContentListView) {
     'use strict';
 
     return {
         version: $.trim(version),
         Collection: Collection,
-        Content: Content
+        Content: Content,
+        ContentListView: ContentListView
     };
 });
