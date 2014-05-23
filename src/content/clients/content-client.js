b5d33e8de82e5cf424775ada327ec55a84d8ce48
Joao-S-Martins <joao@livefyre.com>
2014-05-23 14:30:39 -0700

    Somehow my copy got blasted badly. Fixed now.

--- src/content/clients/content-client.js
+++ src/content/clients/content-client.js
@@ -1,40 +1,32 @@
- * @param opts.contentId {!string} The commentId to fetch content from
- * @param [opts.depthOnly] {boolean} False by default. Set to true to return all
- *      replies to the specified contentId
- * @param [opts.environment] {string} Optional livefyre environment to use dev/prod environment
- * @param callback {function} A callback that is called upon success/failure of the
- *      stream request. Callback signature is "function(error, data)".
+var inherits = require('inherits');
+var LivefyreHttpClient = require('streamhub-sdk/collection/clients/http-client');
+
+'use strict';
+
+/**
+ * Connects to the Livefyre network to grab a single comment state and
+ * related thread states
+ * @param [opts] {object}
+ * @param [opts.serviceName] {string} The StreamHub web service to request
+ * @param [opts.protocol] {string} 'http:' or 'https:'
+ * @extends {LivefyreHttpClient}
+ * @constructor
  */
-LivefyreContentClient.prototype.getContent = function(opts, callback) {
+var LivefyreContentClient = function (opts) {
     opts = opts || {};
-    callback = callback || function() {};
-
-    var url = [
-        this._getUrlBase(opts),
-        "/api/v3.0/content/thread/"
-    ].join("");
-
-    var request = this._request({
-        url: url,
-        data: {
-            collection_id: opts.collectionId,
-            content_id: opts.contentId,
-            depth_only: opts.depthOnly || false
-        }
-    }, function (err, data) {
-        if (err) {
-            return callback.apply(this, arguments);
-        }
-        if (data.status === 'error') {
-            return callback(data.msg);
-        }
-        callback(null, data.data);
-    });
-
-    return request;
+    opts.serviceName = opts.serviceName || this._serviceName;
+    LivefyreHttpClient.call(this, opts);
 };
+inherits(LivefyreContentClient, LivefyreHttpClient);
 
-module.exports = LivefyreContentClient;
+LivefyreContentClient.prototype._serviceName = 'bootstrap';
+
+/**
+ * Fetches a content thread from the livefyre conversation stream with the
+ * supplied arguments. Parents will be provided for content that has parentId.
+ * @param opts {Object} The livefyre collection options.
+ * @param opts.network {!string} The name of the network in the livefyre platform
+ * @param opts.collectionId {!string} The livefyre collectionId for the conversation stream
  * @param opts.contentId {!string} The commentId to fetch content from (default "0")
  * @param [opts.depthOnly] {boolean} False by default. Set to true to return all
  *      replies to the specified contentId
@@ -61,9 +53,6 @@ LivefyreContentClient.prototype.getContent = function(opts, callback) {
     }, function (err, data) {
         if (err) {
             return callback.apply(this, arguments);
-        }
-        if (data.timeout) {
-            return callback(null, { timeout: data.timeout });
         }
         if (data.status === 'error') {
             return callback(data.msg);
